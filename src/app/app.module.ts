33bd5964b1dc622de452b392dda303a19689cebd
Uladzislau Novikau <novikovvlad3005@gmail.com>
2018-05-22 22:54:29 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EditEventComponentComponent } from './edit-event-component/edit-event-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditEventComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/edit-event-component/edit-event-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-event-component',
+  templateUrl: './edit-event-component.component.html',
+  styleUrls: ['./edit-event-component.component.css']
+})
+export class EditEventComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
